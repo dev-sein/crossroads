060833c5b8ee93f792ebfab421fade7ff45fd6f2
dev-sein <seinee114@gmail.com>
2023-03-24 16:14:21 +0900

    모달 주석 삭제

--- src/main/java/com/crossroads/app/mapper/MemberMapper.java
+++ src/main/java/com/crossroads/app/mapper/MemberMapper.java
@@ -1,7 +1,9 @@
 package com.crossroads.app.mapper;
 
+import com.crossroads.app.domain.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
+    public void insert(MemberVO memberVO);
 }
