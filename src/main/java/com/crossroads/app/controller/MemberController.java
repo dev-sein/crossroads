197040bd557e72c08f86bdfaf43ecafda6c7b052
dev-sein <seinee114@gmail.com>
2023-03-25 18:38:08 +0900

    회원가입 테스트

--- src/main/java/com/crossroads/app/controller/MemberController.java
+++ src/main/java/com/crossroads/app/controller/MemberController.java
@@ -1,15 +1,38 @@
 package com.crossroads.app.controller;
 
+import com.crossroads.app.domain.vo.MemberVO;
+import com.crossroads.app.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
     //회원가입
     @GetMapping("join")
     public String join(){
         return "member/join";
     }
+
+    //회원가입 테스트
+    @GetMapping("jointest")
+    public String joinTest(){
+        return "mypage/joinTest";
+    }
+
+    @PostMapping("joinTest")
+    public String signUp(MemberVO memberVO) {
+        memberService.save(memberVO);
+        return "redirect:/login"; //로그인 구현 예정
+    }
+
 
     //로그인
     @GetMapping("login")
