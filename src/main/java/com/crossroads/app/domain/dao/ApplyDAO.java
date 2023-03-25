dce6a06ebfe48190d55869aab6f9adbe6b0a5d3d
SeyunKimdev <seyun.dev@gmail.com>
2023-03-25 16:43:47 +0900

    경로 수정 및 Apply, Mobile 쪽 작업중

--- src/main/java/com/crossroads/app/domain/dao/ApplyDAO.java
+++ src/main/java/com/crossroads/app/domain/dao/ApplyDAO.java
@@ -1,9 +1,20 @@
 package com.crossroads.app.domain.dao;
 
+import com.crossroads.app.domain.dto.ApplyDTO;
+import com.crossroads.app.mapper.ApplyMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class ApplyDAO {
+    private final ApplyMapper applyMapper;
+
+//    신청목록 전체조회
+    public List<ApplyDTO> findAll() {
+        return applyMapper.selectAll();
+    }
+
 }
