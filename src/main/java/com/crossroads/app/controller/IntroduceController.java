9df29366a115095410add4608edc7e855e65c4e6
dev-luk <rladnrtjdsla@gmail.com>
2023-03-27 00:55:13 +0900

    컨트롤러 + 메인페이지

--- src/main/java/com/crossroads/app/controller/IntroduceController.java
+++ src/main/java/com/crossroads/app/controller/IntroduceController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IntroduceController {
     //사업소개
-    @GetMapping("introduce")
+    @GetMapping("/introduce")
     public String introduce(){
         return "introduce/introduce";
     }
     //자주 묻는 질문
-    @GetMapping("notice")
+    @GetMapping("/notice")
     public String notice(){
         return "introduce/notice";
     }
