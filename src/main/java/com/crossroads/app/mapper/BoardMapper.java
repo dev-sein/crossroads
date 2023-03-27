7404729431a250b68e3d7d19a2860014700ca978
dev-mwYoon <dev.mwYoon@gmail.com>
2023-03-27 11:46:04 +0900

    어드민 게시글 목록 조회 Service까지 작업. ReplyVO, ReviewVO 필드명 수정

--- src/main/java/com/crossroads/app/mapper/BoardMapper.java
+++ src/main/java/com/crossroads/app/mapper/BoardMapper.java
@@ -1,7 +1,12 @@
 package com.crossroads.app.mapper;
 
+import com.crossroads.app.domain.dto.BoardDTO;
+import com.crossroads.app.domain.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
+    public List<BoardDTO> selectAllAdmin();
 }
