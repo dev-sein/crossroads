c07270fa26e8b5c2e01ea66e8cceecdd96fdf429
SeyunKimdev <seyun.dev@gmail.com>
2023-03-27 17:38:57 +0900

    김세윤 포인트 작업중

--- src/main/java/com/crossroads/app/mapper/PointMapper.java
+++ src/main/java/com/crossroads/app/mapper/PointMapper.java
@@ -2,6 +2,14 @@ package com.crossroads.app.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface PointMapper {
+
+//  현재 포인트
+    public Long selectPoint(Long memberId);
+
+//  포인트 수정
+    public void updatePoint(Map<String, Object> info);
 }
