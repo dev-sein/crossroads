114ae153c5c2e466a6f5d4576a3ce0a82be9d9ee
dev-sein <seinee114@gmail.com>
2023-03-27 11:18:42 +0900

    아이디 중복체크 수정중

--- src/main/resources/static/js/member/join.js
+++ src/main/resources/static/js/member/join.js
@@ -90,6 +90,31 @@ $idInput.on("blur", function() {
 	}
 });
 
+
+function checkId(){
+	var id = $('#memberIdentification').val(); //id값이 "id"인 입력란의 값을 저장
+	$.ajax({
+		url:'./checkId', //Controller에서 요청 받을 주소
+		type:'post', //POST 방식으로 전달
+		data:{memberIdentification:memberIdentification},
+		success:function(cnt){ //컨트롤러에서 넘어온 cnt값을 받는다
+			if(cnt == 0){ //cnt가 1이 아니면(=0일 경우) -> 사용 가능한 아이디
+				$('.id_ok').css("display","inline-block");
+				$('.id_already').css("display", "none");
+			} else { // cnt가 1일 경우 -> 이미 존재하는 아이디
+				$('.id_already').css("display","inline-block");
+				$('.id_ok').css("display", "none");
+				alert("아이디를 다시 입력해주세요");
+				$('#memberIdentification').val('');
+			}
+		},
+		error:function(){
+			alert("에러입니다");
+		}
+	});
+};
+
+
 // 비밀번호 정규식 이벤트 사용 및 함수
 $passwordInput.on("blur", function() {
 	var $passwordInputValue = $passwordInput.val();
