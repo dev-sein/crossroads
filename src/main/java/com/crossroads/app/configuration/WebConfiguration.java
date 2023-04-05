0c2639d775c549f97785db33c566c8f0b68d723c
dev-sein <seinee114@gmail.com>
2023-04-05 15:58:30 +0900

    연수신청 세션 수정 완료

---
+++ src/main/java/com/crossroads/app/configuration/WebConfiguration.java
@@ -0,0 +1,21 @@
+/*
+package com.crossroads.app.configuration;
+
+import com.crossroads.app.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfgurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/member/login");
+
+//        registry.addInterceptor(new ApplyInterceptor())
+//                .addPathPatterns("/apply/**");
+    }
+}
+*/
