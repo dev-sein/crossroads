ec81c5ab9b8b8fa5a2e20165752cc9aa606cd97e
dev-luk <rladnrtjdsla@gmail.com>
2023-04-05 19:47:10 +0900

    수정?

--- src/main/java/com/crossroads/app/Config/WebConfig.java
+++ src/main/java/com/crossroads/app/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/D:/uploads/");
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:/C:/upload/");
     }
 }
